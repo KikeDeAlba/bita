a0f685b5ac421a911c81839c59e4bed7b0d4f136
Jesus Enrique De Alba Gaytan <enriquedealba@solemti.com>
2026-09-22 11:09:35 -0700

    chore(release): 0.5.1

--- src/cli/router.ts
+++ src/cli/router.ts
@@ -19,7 +19,7 @@ import { runLink } from './commands/link.ts'
 import { runCancel, runCurrent, runLog, runStart, runStop } from './commands/timer.ts'
 import { writeOut } from './output.ts'
 
-export const VERSION = '0.5.0'
+export const VERSION = '0.5.1'
 
 const HELP = `bita ${VERSION}
 
